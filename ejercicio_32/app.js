b409b1cefda9a77565c2b869298cdd376eeaaa5e
Victor <vmmrcas98@gmail.com>
2024-12-17 11:15:18 +0100

    ,

--- ejercicio_32/app.js
+++ ejercicio_32/app.js
@@ -1,7 +1,17 @@
 const URL='https://fpaniaguajavascript.github.io/movies-250.json';
 
 function processMovie(data) {
-    console.log(data);
+    const peliculas= data.movies
 }
+//Recorremos con el metodo for each
+peliculas.forEach(pelicula => {
+    console.log(pelicula.title)
+});
+//Recorremos con el bucle for-of
+
+for(pelicula of peliculas){
+    console.log("Director:" +  pelicula.director)
+}
+
 
 doGetRequest(URL, processMovie);
